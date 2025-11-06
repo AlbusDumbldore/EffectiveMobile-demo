ee6b51d1278a9e75aa796724b0a2ba94f6a94499
Sergey Sorokin <albuspersiv19@gmail.com>
2025-11-06 12:20:39 +0300

    feat: Подключен сервер и добавлены переменные окружения

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,14 @@
-console.log(1234);
+import 'reflect-metadata';
+import express from 'express';
+import { appConfig } from './config';
+import { setupSwagger } from './swagger/setup-swagger';
+
+const bootstrap = async () => {
+  const server = express();
+
+  setupSwagger(server);
+
+  server.listen(appConfig.port);
+};
+
+bootstrap();
